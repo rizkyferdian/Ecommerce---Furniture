79c11bb7d9e7d717c7339b3cc1d12be3b19afb0d
rizky ferdian <rizkyferdian002@gmail.com>
2022-08-22 19:50:57 +0700

    Navigation Product for Admin

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -12,9 +12,15 @@
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard')">
+                    <x-jet-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard.index')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+
+                    @if (Auth::user()->roles == 'ADMIN')
+                        <x-jet-nav-link href="{{ route('dashboard.product.index') }}" :active="request()->routeIs('dashboard.product.index')">
+                            {{ __('Products') }}
+                        </x-jet-nav-link>
+                    @endif
                 </div>
             </div>
 
@@ -154,6 +160,12 @@
             <x-jet-responsive-nav-link href="{{ route('dashboard.index') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-jet-responsive-nav-link>
+
+            @if (Auth::user()->roles == 'ADMIN')
+                <x-jet-nav-link href="{{ route('dashboard.product.index') }}" :active="request()->routeIs('dashboard.product.index')">
+                    {{ __('Products') }}
+                </x-jet-nav-link>
+            @endif
         </div>
 
         <!-- Responsive Settings Options -->
